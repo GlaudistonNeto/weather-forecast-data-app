3e765fcd67aa8bd3508e785782d6b6c90668b31e
glaudistonneto <glaudistonson@gmail.com>
2025-04-04 21:41:19 -0300

    finished frontend and backend as also placed both together

--- backend.py
+++ backend.py
@@ -1,21 +1,17 @@
 import requests
 
-API_KEY = "5ce4a9ee41fc00e16ed025003ee9bb0f" # ********************************
+API_KEY = "********************************"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"https://pro.openweathermap.org/data/2.5/forecast/hourly?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 24 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky-Condition":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return  filtered_data
 
 
-if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+if __name__=="__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
